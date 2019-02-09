0fb20ec684db72e9bf4d6d1ad494733a76175aa8
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-02-10 00:57:52 +0100

    added temp database save

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -19,5 +21,22 @@ class ArticleController extends Controller {
         $articles = ['Article 1', 'Article 2'];
 
         return $this->render('articles/index.html.twig', array ("articles"=>$articles));
+    }
+
+    /**
+     * @Route("/article/save")
+     */
+    public function save(){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('Article One');
+        $article->setBody("This is the body for article one.");
+
+        $entityManager->presist($article);
+
+        $entityManager->flush();
+
+        return new Response('Saves an article with the id of '.$article->getId());
     }
 }
