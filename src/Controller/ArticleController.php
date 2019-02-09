4812576d00cb73e108d4c0eac0489efd44497a79
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-02-09 11:45:35 +0100

    annotations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 
 class ArticleController {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
     public function index(){
         return new Response('<html><body>Hello World</body></html>');
     }
